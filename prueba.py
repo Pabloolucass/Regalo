b72de88aff764590d1775440b15abb85b6663ba0
Pabloolucass <134289625+Pabloolucass@users.noreply.github.com>
2024-11-07 19:22:22 +0100

    Add files via upload

---
+++ prueba.py
@@ -0,0 +1,20 @@
+cant = 1.41
+blur1 = 156
+blur2 = 156
+id_cheque = '76696e696c6f'
+beneficiario = 'sara ripoll moreno'
+dinero = 40
+usado = False
+
+movimientos = {
+    'Vuelo Valencia_Mallorca - 04/10/2024': -15.00,
+    'Autobús Alicante_Granada - 02/07/2024': -3.72,
+    'Hotel Zielo de Levante - 05/02/2024': -40.00,
+    'Ryanair Valencia_Viena - 15/12/2023': -98.00,
+    'Renfe Alicante_Valencia - 05/09/2023': -20.00,
+    'Amanecer - 15/07/2023': -0.0,
+    'Fuegos artificiales - 25/07/2023' : -5.5,
+    'Autobús hogueras - 24/06/2023': -1.45,
+    'Copity PAU - 08/06/2023': -6.00,
+    'Ruta del bacalao - 29/12/2022': -5.5
+}
