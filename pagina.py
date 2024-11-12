7a97febc9b77f5acf8c6a773024a5237ff86f096
Pabloolucass <134289625+Pabloolucass@users.noreply.github.com>
2024-11-12 13:03:28 +0100

    Update pagina.py

--- pagina.py
+++ pagina.py
@@ -6,6 +6,10 @@ import pandas as pd
 import pydeck as pdk
 from PIL import ImageFilter, Image
 import prueba
+import smtplib
+from email.message import EmailMessage
+import ssl
+import os
 
 # Contraseña de acceso
 PASSWORD = "mirando las estrellas me acordé de ti"
@@ -15,11 +19,11 @@ clave = PASSWORD.split()
 lat, lon = 39.56939, 2.65024  # Zielo de levante
 
 # Fecha y hora objetivo (14 de noviembre a las 00:00)
-target_date = datetime(2024, 11, 14, 20, 30, 0)
+target_date = datetime(2024, 11, 14, 21, 30, 0)
 
 # image, col_menu = st.columns((0.2,0.8))
 
-pistas = ['Inicio', "M.L.E.M.A.D.T. BANK",'PHOTO RESTORATION', "MLEMADT", '...']
+pistas = ['INICIO',"MLEMADT", "M.L.E.M.A.D.T. BANK",'MLEMADT EXPRESS','PHOTO RESTORATION',   '...']
 
 tienda = {
     'Enfocar imagen 50%': 40,
@@ -29,6 +33,39 @@ tienda = {
     'Pasar imagen a color': 46
 }
 
+def send_mail(email_sender, password, email_receiver, subject, body, attachment_path=None):
+    """
+    Envía un correo electrónico con la opción de adjuntar un archivo.
+
+    Parameters:
+    - email_sender: dirección de correo del remitente.
+    - password: contraseña del remitente.
+    - email_receiver: dirección de correo del receptor.
+    - subject: asunto del correo.
+    - body: cuerpo del mensaje del correo.
+    - attachment_path: ruta del archivo adjunto (opcional).
+    """
+    em = EmailMessage()
+    em['From'] = email_sender
+    em['To'] = email_receiver
+    em['Subject'] = subject
+    em.set_content(body)
+
+    # Adjuntar archivo si se proporciona la ruta
+    if attachment_path and os.path.isfile(attachment_path):
+        with open(attachment_path, 'rb') as file:
+            file_data = file.read()
+            file_name = os.path.basename(attachment_path)
+            em.add_attachment(file_data, maintype='application', subtype='octet-stream', filename=file_name)
+
+    # Establecer contexto SSL para la conexión segura
+    context = ssl.create_default_context()
+
+    # Enviar el correo
+    with smtplib.SMTP_SSL('smtp.gmail.com', 465, context=context) as smtp:
+        smtp.login(email_sender, password)
+        smtp.send_message(em)
+    print("Correo enviado exitosamente")
 
 #difuminar imagen
 def load_and_blur_image(image_path, blur_radius=5):
@@ -41,7 +78,7 @@ def main():
         selected = option_menu(
             menu_title="",
             options= pistas,
-            icons=['tic',"money", "tic", "tic", 'tic'],
+            icons=['tic',"money", "tic", "tic", 'tic', 'tic'],
             menu_icon= 'tic',
             default_index=0
         )
@@ -55,7 +92,7 @@ def main():
 
         left_co, cent_co,last_co = st.columns(3)
         with cent_co:
-            st.image('luna 84% buena.png', caption= '84%')
+            st.image('luna 32% buena.png', caption= '32%')
         st.title('')
 
         cara1, cara2 = st.columns(2)
@@ -112,83 +149,8 @@ def main():
                 countdown_container.markdown("<h1 style='text-align: center; color: green;'>¡La cuenta regresiva ha terminado!</h1>", unsafe_allow_html=True)
                 break
     
+
     if selected == pistas[1]:
-        
-        # CSS para estilo personalizado
-        st.markdown(
-        """
-        <style>
-            .header {
-                text-align: center;
-                font-size: 24px;
-                font-weight: bold;
-                margin-bottom: 20px;
-            }
-            .balance-container {
-                text-align: center;
-                font-size: 36px;
-                font-weight: bold;
-                color: black;
-                margin: 10px 0;
-            }
-            .amount {
-                text-align: center;
-                font-size: 40px;
-                color: #000;
-            }
-            .icon-container {
-                text-align: right;
-                margin-top: -50px;
-                margin-right: 10px;
-            }
-            .icon {
-                font-size: 24px;
-                margin-left: 5px;
-                color: #888;
-            }
-        </style>
-        """,
-        unsafe_allow_html=True
-        )
-        st.image('banco.png') 
-        page = st.radio("Selecciona una opción:", ['Inicio', "Saldo y movimientos", "Reclamar cheque"])
-
-        if page == "Reclamar cheque":
-            st.subheader("Formulario para reclamar cheque")
-
-            # Ingresar ID y nombre de la persona
-            id_cheque = st.text_input("Ingresa el ID del cheque")
-            nombre_persona = st.text_input("Ingresa el nombre del beneficiario (Nombre y apellidos)").lower()
-
-            if st.button("Enviar reclamo"):
-                if (id_cheque == prueba.id_cheque) and (nombre_persona == prueba.beneficiario) and(prueba.usado == False):
-                    st.success(f"Cheque de {prueba.dinero}€ reclamado con éxito por {nombre_persona} (ID: {id_cheque}).")
-                    prueba.cant += prueba.dinero
-                    prueba.movimientos['Cheque COM - 07/11/2024'] =  prueba.dinero
-                    prueba.usado = True
-                else:
-                    st.error("Por favor, revisa que la información sea correcta.")
-        elif page == 'Inicio':
-            pass
-            
-        elif page == 'Saldo y movimientos':
-            # Mostrar la cabecera
-            st.markdown("<div class='header'>Saldo</div>", unsafe_allow_html=True)
-
-            # Mostrar el nombre del titular y el saldo
-            st.markdown("<div class='balance-container'>SARA RIPOLL</div>", unsafe_allow_html=True)
-            st.markdown('---')
-            st.markdown(f"<div class='amount'>{prueba.cant:.2f} €</div>", unsafe_allow_html=True)
-            st.markdown('---')
-                    
-            st.markdown("<div class='header'>Movimientos</div>", unsafe_allow_html=True)
-            st.markdown('---')
-            for mov, dinero in prueba.movimientos.items():
-                st.subheader(mov)
-                st.subheader(f'{dinero} €')
-                st.markdown('---')
-
-    if selected == pistas[3]:
         
         m= st.text_input('M')
         l= st.text_input('L')
@@ -253,7 +215,183 @@ def main():
                 with cent:
                     st.markdown('Dónde todo empieza')
 
+
     if selected == pistas[2]:
+        
+        # CSS para estilo personalizado
+        st.markdown(
+        """
+        <style>
+            .header {
+                text-align: center;
+                font-size: 24px;
+                font-weight: bold;
+                margin-bottom: 20px;
+            }
+            .balance-container {
+                text-align: center;
+                font-size: 36px;
+                font-weight: bold;
+                color: black;
+                margin: 10px 0;
+            }
+            .amount {
+                text-align: center;
+                font-size: 40px;
+                color: #000;
+            }
+            .icon-container {
+                text-align: right;
+                margin-top: -50px;
+                margin-right: 10px;
+            }
+            .icon {
+                font-size: 24px;
+                margin-left: 5px;
+                color: #888;
+            }
+        </style>
+        """,
+        unsafe_allow_html=True
+        )
+        st.image('banco.png') 
+        page = st.radio("Selecciona una opción:", ['Inicio', "Saldo y movimientos", "Reclamar cheque"])
+
+        if page == "Reclamar cheque":
+            st.subheader("Formulario para reclamar cheque")
+
+            # Ingresar ID y nombre de la persona
+            id_cheque = st.text_input("Ingresa el ID del cheque")
+            nombre_persona = st.text_input("Ingresa el nombre del beneficiario (Nombre y apellidos)").lower()
+
+            if st.button("Enviar reclamo"):
+                if (id_cheque == prueba.id_cheque) and (nombre_persona == prueba.beneficiario) and(prueba.usado == False) and (not prueba.usado):
+                    st.success(f"Cheque de {prueba.dinero}€ reclamado con éxito por {nombre_persona} (ID: {id_cheque}).")
+                    prueba.cant += prueba.dinero
+                    prueba.movimientos['Cheque COM - 07/11/2024'] =  prueba.dinero
+                    prueba.usado = True
+                else:
+                    st.error("Por favor, revisa que la información sea correcta.")
+        elif page == 'Inicio':
+            pass
+            
+        elif page == 'Saldo y movimientos':
+            # Mostrar la cabecera
+            st.markdown("<div class='header'>Saldo</div>", unsafe_allow_html=True)
+
+            # Mostrar el nombre del titular y el saldo
+            st.markdown("<div class='balance-container'>SARA RIPOLL</div>", unsafe_allow_html=True)
+            st.markdown('---')
+            st.markdown(f"<div class='amount'>{prueba.cant:.2f} €</div>", unsafe_allow_html=True)
+            st.markdown('---')
+                    
+            st.markdown("<div class='header'>Movimientos</div>", unsafe_allow_html=True)
+            st.markdown('---')
+
+            for mov, dinero in prueba.movimientos.items():
+                st.subheader(mov)
+                st.subheader(f'{dinero} €')
+                st.markdown('---')
+
+
+    if selected == pistas[3]:
+            st.title('')
+            st.title('')
+            st.image('MLEMADT Express.png', width= 650)
+            pestaña = st.radio("Selecciona una opción:", ['Inicio', "Seguimiento pedido"])
+            
+            if pestaña == 'Seguimiento pedido':
+                ID = st.text_input('Introduzca el ID de seguimiento')
+                if st.button('Ingresar') and (ID == prueba.ID):
+                    st.write(f"**ID de seguimiento:** {ID}")
+                    st.subheader('')
+
+                    st.markdown("""
+                    <style>
+                        .progress-container {
+                            display: flex;
+                            justify-content: space-around;
+                            align-items: center;
+                            margin-top: 20px;
+                            font-weight: bold;
+                        }
+                        .progress-step {
+                            text-align: center;
+                            color: gray;
+                            position: relative;
+                            padding: 10px;
+                        }
+                        .progress-step.completed {
+                            color: green;
+                        }
+                        .progress-circle {
+                            width: 20px;
+                            height: 20px;
+                            border-radius: 50%;
+                            background-color: lightgray;
+                            display: flex;
+                            align-items: center;
+                            justify-content: center;
+                            margin: auto;
+                            margin-bottom: 5px;
+                        }
+                        .progress-circle.completed {
+                            background-color: green;
+                        }
+                        .progress-line {
+                            flex-grow: 1;
+                            height: 5px;
+                            background-color: lightgray;
+                            margin: 0 5px;
+                        }
+                        .progress-line.completed {
+                            background-color: green;
+                        }
+                    </style>
+                    <div class="progress-container">
+                        <div class="progress-step">
+                            <div class="progress-circle completed"></div>
+                            Preparando Pedido
+                        </div>
+                        <div class="progress-line "></div>
+                        <div class="progress-step">
+                            <div class="progress-circle "></div>
+                            En Tránsito
+                        </div>
+                        <div class="progress-line"></div> <!-- Not completed -->
+                        <div class="progress-step">
+                            <div class="progress-circle"></div> <!-- Not completed -->
+                            En Reparto
+                        </div>
+                        <div class="progress-line"></div> <!-- Not completed -->
+                        <div class="progress-step">
+                            <div class="progress-circle"></div> <!-- Not completed -->
+                            Entregado
+                        </div>
+                    </div>
+                    """, unsafe_allow_html=True)
+                    
+                    estimated_delivery_date = "14/11/2024"
+                    st.subheader('')
+                    st.write(f"**Fecha estimada de entrega:** {estimated_delivery_date}")
+                    st.subheader('')
+
+                    # Historial del envío
+                    st.write("### Historial de envío")
+                    col1, col2 = st.columns(2)
+                    for event in prueba.shipping_history:
+                        with col1:
+                            st.markdown('---')                        
+                            st.write(f"{event['date']} - {event['time']} {event['icon']} **{event['status']}**")
+                            st.subheader('')
+                            st.caption(f"{event['location']}")
+                        with col2:
+                            st.subheader('')
+                            st.image(event['image'], width=200)
+                else:
+                    pass
+
+    if selected == pistas[4]:
         left_co, middle,last_co = st.columns(3)
         with left_co:
             st.image('rest fotos logo.png', width= 580,)
@@ -322,13 +460,9 @@ def main():
         with s7:
             st.button('Pag 9')
 
+    
 
-    if selected == pistas[4]:
-        
-        pass
-            
-
-    if selected == pistas[4]:
+    if selected == pistas[5]:
         st.title('??????????????????????????????????????????????????????????????????????????????????????????????????????????????????????????????????????????????????????????????????????????????????????????????????????????????????????????????????????????????????????????????????????????????????????????????????????????????????????????????????????????????????????????????????????????????????????????????????????????????????????????????????????????????????????????')
 
 # def login():
